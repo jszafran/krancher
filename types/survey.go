eb80f0cd6ab1452d7d2f58ac219afe63943d041a
jszafran <jszafran.pv@gmail.com>
2022-07-15 17:19:19 +0200

    Move sorting logic to separate function.

--- types/survey.go
+++ types/survey.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"encoding/csv"
+	"errors"
 	"log"
 	"math"
 	"os"
@@ -74,6 +75,45 @@ func buildIndex(org OrgStructure, dataNodes []string) orgNodeIndex {
 		ixData[node] = l
 	}
 	return orgNodeIndex{ixData}
+}
+
+func sortDataByOrgNode(data [][]string, orgColIx int) ([][]string, error) {
+	parseNode := func(node string) ([]int, error) {
+		node = strings.Replace(node, "N", "", -1)
+		spl := strings.Split(node, ".")
+		if spl[len(spl)-1] == "" {
+			spl = spl[:len(spl)-1]
+		}
+		nodeInts := make([]int, len(spl))
+		for i, el := range spl {
+			nodeInt, err := strconv.Atoi(el)
+			if err != nil {
+				return nil, errors.New("error when parsing node " + node + " to integers")
+			}
+			nodeInts[i] = nodeInt
+		}
+		return nodeInts, nil
+	}
+
+	sort.SliceStable(data, func(i, j int) bool {
+		n1, err1 := parseNode(data[i][orgColIx])
+		n2, err2 := parseNode(data[j][orgColIx])
+		if err1 != nil || err2 != nil {
+			if err1 != nil {
+				log.Fatal(err1)
+			}
+			log.Fatal(err2)
+		}
+		minLen := int(math.Min(float64(len(n1)), float64(len(n2))))
+		for i := 0; i < minLen; i++ {
+			if n1[i] == n2[i] {
+				continue
+			}
+			return n1[i] < n2[i]
+		}
+		return false
+	})
+	return data, nil
 }
 
 func NewSurvey(dataPath string, s Schema, org OrgStructure) Survey {
