1ae5f931a511012859da6e7d62e40948d818c3b3
jszafran <jszafran.pv@gmail.com>
2022-08-11 21:48:31 +0200

    Add basic parsing for workload request.

--- cmd/http/routes.go
+++ cmd/http/routes.go
@@ -1,13 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"krancher/survey"
 	"net/http"
 )
 
 type WorkloadRequest struct {
-	Algorithm string `json:"algorithm"`
+	Algorithm        string `json:"algorithm"`
+	DataPath         string `json:"data_path"`
+	OutputPath       string `json:"output_path"`
+	SchemaPath       string `json:"schema_path"`
+	OrgStructurePath string `json:"org_structure_path"`
+	WorkloadPath     string `json:"workload_path"`
+}
+
+func (w *WorkloadRequest) IsValid() bool {
+	if w.Algorithm == "" {
+		return false
+	}
+
+	_, err := survey.ProcessingAlgorithmFactory(w.Algorithm)
+	if err != nil {
+		return false
+	}
+
+	if w.DataPath == "" {
+		return false
+	}
+
+	if w.OutputPath == "" {
+		return false
+	}
+
+	if w.SchemaPath == "" {
+		return false
+	}
+
+	if w.OrgStructurePath == "" {
+		return false
+	}
+
+	if w.WorkloadPath == "" {
+		return false
+	}
+
+	return true
 }
 
 type WorkloadResponse struct {
@@ -15,7 +55,21 @@ type WorkloadResponse struct {
 }
 
 func CreateWorkloadHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "processing workload")
+	var request WorkloadRequest
+	d := json.NewDecoder(r.Body)
+	d.DisallowUnknownFields()
+	err := d.Decode(&request)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	if !request.IsValid() {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "processing request")
 }
 
 func ListWorkloadsHandler(w http.ResponseWriter, r *http.Request) {
